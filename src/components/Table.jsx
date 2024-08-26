85b5135f72f31cc41443bf03c982e02dd410a745
Katherine Hanrahan <katherinehanrahan@Corins-MBP.lan>
2024-08-25 20:37:59 -0500

    Task 5 Completed

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,27 @@
+import satData from "./satData";
+
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sat.map((satData, id) => (
+          <tr key={id}>
+            <td>{satData.name}</td>
+            <td>{satData.type}</td>
+            <td>{satData.launchDate}</td>
+            <td>{satData.operational ? "Active" : "Inactive"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
