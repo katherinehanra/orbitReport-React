f6d542e2793cc0070da698ee7f216f2289cd4f45
Katherine Hanrahan <katherinehanrahan@Corins-MBP.lan>
2024-08-25 19:07:50 -0500

    Graded Assignment 6 - Task 2 Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+//import { useState } from "react";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
