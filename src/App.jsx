7cd30ad199dea8d432a25f7312fe989e6991552f
Katherine Hanrahan <katherinehanrahan@Corins-MBP.lan>
2024-08-26 20:09:39 -0500

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ import Banner from "./components/Banner";
 function App() {
   const [sat, setSat] = useState(satData);
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  };
   return (
     <div>
       <Banner />
@@ -19,12 +25,5 @@ function App() {
     </div>
   );
 }
-
-const filterByType = (currentType) => {
-  const displaySats = satData.filter((newSatDisplay) => {
-    return newSatDisplay.orbitType === currentType;
-  });
-  setSat(displaySats);
-};
 
 export default App;
